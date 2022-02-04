fb42e38a5e82fdcc22d4f0231124d06daadb4d68
davelopez <46503462+davelopez@users.noreply.github.com>
2022-02-04 15:51:26 +0100

    Implement cleanWorkflowDocument

--- server/src/commands/cleanWorkflow.ts
+++ server/src/commands/cleanWorkflow.ts
@@ -1,5 +1,5 @@
 import { RequestType } from "vscode-languageserver";
-import { WorkflowDocument } from "../languageTypes";
+import { ASTNode, PropertyASTNode, WorkflowDocument } from "../languageTypes";
 import { GalaxyWorkflowLanguageServer } from "../server";
 import { CustomCommand, LSRequestIdentifiers } from "./common";
 
@@ -17,6 +17,8 @@ export namespace CleanWorkflowDocumentRequest {
     LSRequestIdentifiers.CLEAN_WORKFLOW
   );
 }
+
+const CLEANABLE_PROPERTY_NAMES = new Set(["position", "uuid", "errors", "version"]);
 
 export class CleanWorkflowCommand extends CustomCommand {
   public static register(server: GalaxyWorkflowLanguageServer): CleanWorkflowCommand {
@@ -38,10 +40,65 @@ export class CleanWorkflowCommand extends CustomCommand {
   }
 
   private async cleanWorkflowDocument(workflowDocument: WorkflowDocument): Promise<CleanWorkflowDocument> {
-    const contents = workflowDocument.textDocument.getText().replace("position", "");
+    const nodesToRemove = this.getNonEssentialNodes(workflowDocument.jsonDocument.root);
+    const contents = this.getCleanContents(workflowDocument.textDocument.getText(), nodesToRemove.reverse());
     const result: CleanWorkflowDocument = {
       contents: contents,
     };
+    return result;
+  }
+
+  private getNonEssentialNodes(root: ASTNode | undefined): PropertyASTNode[] {
+    if (!root) {
+      return [];
+    }
+    const result: PropertyASTNode[] = [];
+    const toVisit: { node: ASTNode }[] = [{ node: root }];
+    let nextToVisit = 0;
+
+    const collectNonEssentialProperties = (node: ASTNode) => {
+      if (node.type === "array") {
+        node.items.forEach((node) => {
+          if (node) {
+            toVisit.push({ node });
+          }
+        });
+      } else if (node.type === "object") {
+        node.properties.forEach((property: PropertyASTNode) => {
+          const key = property.keyNode.value;
+          if (CLEANABLE_PROPERTY_NAMES.has(key)) {
+            result.push(property);
+          }
+          if (property.valueNode) {
+            toVisit.push({ node: property.valueNode });
+          }
+        });
+      }
+    };
+
+    while (nextToVisit < toVisit.length) {
+      const next = toVisit[nextToVisit++];
+      collectNonEssentialProperties(next.node);
+    }
+
+    return result;
+  }
+
+  private getCleanContents(documentText: string, nodesToRemove: ASTNode[]): string {
+    const removeChunks: string[] = [];
+    let result = documentText;
+    nodesToRemove.forEach((node) => {
+      let startPos = node.offset;
+      let endPos = node.offset + node.length;
+      startPos = documentText.lastIndexOf("\n", startPos);
+      if (documentText.charAt(endPos) === ",") {
+        endPos++;
+      }
+      removeChunks.push(documentText.substring(startPos, endPos));
+    });
+    removeChunks.forEach((chunk) => {
+      result = result.replace(chunk, "");
+    });
     return result;
   }
 }
