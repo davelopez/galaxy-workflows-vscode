3b583ca19e3fa35d1c714c372de903a84b89ef2a
davelopez <46503462+davelopez@users.noreply.github.com>
2022-06-05 16:32:54 +0200

    Add unit test for `getStepNodes`

---
+++ server/tests/unit/nativeWorkflowDocument.test.ts
@@ -0,0 +1,17 @@
+import { createNativeWorkflowDocument } from "../testHelpers";
+import { TestWorkflowProvider } from "../testWorkflowProvider";
+
+describe("NativeWorkflowDocument", () => {
+  describe("getStepNodes", () => {
+    it.each([
+      ["", 0],
+      [TestWorkflowProvider.nativeJson.validation.withoutSteps, 0],
+      [TestWorkflowProvider.nativeJson.validation.withOneStep, 1],
+      [TestWorkflowProvider.nativeJson.validation.withThreeSteps, 3],
+    ])("returns the expected number of steps", (wf_content: string, expectedNumSteps: number) => {
+      const wfDocument = createNativeWorkflowDocument(wf_content);
+      const stepNodes = wfDocument.getStepNodes();
+      expect(stepNodes).toHaveLength(expectedNumSteps);
+    });
+  });
+});
