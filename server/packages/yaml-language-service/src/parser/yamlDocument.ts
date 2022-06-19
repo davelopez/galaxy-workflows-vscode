72ae2b884ab6ac00073a98f5b2f077b87565e56b
davelopez <46503462+davelopez@users.noreply.github.com>
2022-06-19 16:22:00 +0200

    Add comment lines to parsed document

--- server/packages/yaml-language-service/src/parser/yamlDocument.ts
+++ server/packages/yaml-language-service/src/parser/yamlDocument.ts
@@ -1,23 +1,65 @@
 import { TextDocument } from "vscode-languageserver-textdocument";
 import { Diagnostic, DiagnosticSeverity, Position } from "vscode-languageserver-types";
-import { Document, YAMLError, YAMLWarning } from "yaml";
+import { Document, Node, visit, YAMLError, YAMLWarning } from "yaml";
 import { TextBuffer } from "../utils/textBuffer";
 import { ASTNode } from "./astTypes";
 
 const FULL_LINE_ERROR = true;
 const YAML_SOURCE = "YAML";
 
+export class LineComment {
+  constructor(public readonly text: string) {}
+}
+
 export class YAMLSubDocument {
+  private _lineComments: LineComment[] | undefined;
+
   constructor(public readonly root: ASTNode | undefined, private readonly parsedDocument: Document) {}
 
   get errors(): YAMLError[] {
     return this.parsedDocument.errors;
   }
+
   get warnings(): YAMLWarning[] {
     return this.parsedDocument.warnings;
   }
+
+  get lineComments(): LineComment[] {
+    if (!this._lineComments) {
+      this._lineComments = this.collectLineComments();
+    }
+    return this._lineComments;
+  }
+
+  private collectLineComments(): LineComment[] {
+    const lineComments = [];
+    if (this.parsedDocument.commentBefore) {
+      const comments = this.parsedDocument.commentBefore.split("\n");
+      comments.forEach((comment) => lineComments.push(new LineComment(`#${comment}`)));
+    }
+    visit(this.parsedDocument, (_key, docNode) => {
+      const node = docNode as Node;
+      if (node?.commentBefore) {
+        const comments = node?.commentBefore.split("\n");
+        comments.forEach((comment) => lineComments.push(new LineComment(`#${comment}`)));
+      }
+
+      if (node?.comment) {
+        lineComments.push(new LineComment(`#${node.comment}`));
+      }
+    });
+
+    if (this.parsedDocument.comment) {
+      lineComments.push(new LineComment(`#${this.parsedDocument.comment}`));
+    }
+    return lineComments;
+  }
 }
 
+/**
+ * Represents a YAML document.
+ * YAML documents can contain multiple sub-documents separated by "---".
+ */
 export class YAMLDocument {
   private readonly _textBuffer: TextBuffer;
   private _diagnostics: Diagnostic[] | undefined;
@@ -27,10 +69,12 @@ export class YAMLDocument {
     this._diagnostics = undefined;
   }
 
-  public get firstDocument(): YAMLSubDocument | undefined {
+  /** The first or single sub-document parsed. */
+  public get mainDocument(): YAMLSubDocument | undefined {
     return this.subDocuments.at(0);
   }
 
+  /** Returns basic YAML syntax errors or warnings. */
   public get syntaxDiagnostics(): Diagnostic[] {
     if (!this._diagnostics) {
       this._diagnostics = this.getSyntaxDiagnostics();
@@ -38,6 +82,12 @@ export class YAMLDocument {
     return this._diagnostics;
   }
 
+  /** List of comments in this document. */
+  public get lineComments(): LineComment[] {
+    return this.collectLineComments();
+  }
+
+  /** Collects all syntax errors and warnings found on this document. */
   private getSyntaxDiagnostics(): Diagnostic[] {
     const syntaxErrors = this.subDocuments.flatMap((subDoc) =>
       subDoc.errors.map((e) => this.YAMLErrorToDiagnostics(e))
@@ -48,6 +98,7 @@ export class YAMLDocument {
     return syntaxErrors.concat(syntaxWarnings);
   }
 
+  /** Converts from internal YAMLError to a document Diagnostic item. */
   private YAMLErrorToDiagnostics(error: YAMLError): Diagnostic {
     const begin = error.pos[0];
     const end = error.pos[1];
@@ -60,7 +111,15 @@ export class YAMLDocument {
         ? Position.create(start.line, this._textBuffer.getLineLength(start.line))
         : this.textDocument.positionAt(end),
     };
-
     return Diagnostic.create(range, error.message, severity, error.code, YAML_SOURCE);
+  }
+
+  /** Collects all comment lines across all sub-documents contained in this document. */
+  private collectLineComments(): LineComment[] {
+    const lineComments: LineComment[] = [];
+    this.subDocuments.forEach((subDocument) => {
+      lineComments.push(...subDocument.lineComments);
+    });
+    return lineComments;
   }
 }
