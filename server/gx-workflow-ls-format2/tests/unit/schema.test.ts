eb15c167886956171e97ec37d2bb66e4bcf6a201
davelopez <46503462+davelopez@users.noreply.github.com>
2022-07-22 17:34:29 +0200

    Add some tests for schema handling

---
+++ server/gx-workflow-ls-format2/tests/unit/schema.test.ts
@@ -0,0 +1,44 @@
+import { NodePath } from "@gxwf/server-common/src/ast/types";
+import { SchemaNodeResolver } from "../../src/schema/schemaNodeResolver";
+import { GalaxyWorkflowFormat2SchemaLoader } from "../../src/schema/schemaLoader";
+
+describe("Gxformat2 Schema Handling", () => {
+  describe("Schema Version 19_09", () => {
+    let schemaLoader: GalaxyWorkflowFormat2SchemaLoader;
+    beforeAll(() => {
+      schemaLoader = new GalaxyWorkflowFormat2SchemaLoader();
+    });
+    describe("GalaxyWorkflowFormat2SchemaLoader", () => {
+      it("should load all schema record definitions", () => {
+        expect(schemaLoader.definitions.records.size).toBe(25);
+      });
+    });
+
+    describe("SchemaNodeResolver", () => {
+      let nodeResolver: SchemaNodeResolver;
+      beforeAll(() => {
+        nodeResolver = new SchemaNodeResolver(schemaLoader.definitions);
+      });
+      describe("resolveSchemaContext", () => {
+        it.each([
+          [["class"], "class"],
+          [["inputs"], "inputs"],
+          [["inputs", 0], "InputParameter"],
+          [["inputs", 0, "doc"], "doc"],
+          [["inputs", "input1"], "InputParameter"],
+          [["inputs", "input1", "label"], "label"],
+          [["outputs"], "outputs"],
+          [["outputs", 1], "OutputParameter"],
+          [["outputs", "output1"], "OutputParameter"],
+          [["steps"], "steps"],
+          [["steps", 5], "WorkflowStep"],
+          [["steps", "random", "tool_id"], "tool_id"],
+        ])("returns expected schema node name from path", (nodePath: NodePath, expectedNodeName: string) => {
+          const schemaNode = nodeResolver.resolveSchemaContext(nodePath);
+          expect(schemaNode).toBeDefined();
+          expect(schemaNode?.name).toBe(expectedNodeName);
+        });
+      });
+    });
+  });
+});
