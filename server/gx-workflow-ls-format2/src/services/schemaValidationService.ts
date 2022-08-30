87796485ce3480ba00f056fde0bd684c6b42960a
davelopez <46503462+davelopez@users.noreply.github.com>
2022-08-31 01:14:00 +0200

    Refactor schema validation service

--- server/gx-workflow-ls-format2/src/services/schemaValidationService.ts
+++ server/gx-workflow-ls-format2/src/services/schemaValidationService.ts
@@ -1,76 +1,89 @@
 import { ASTNodeManager } from "@gxwf/server-common/src/ast/nodeManager";
-import { ASTNode } from "@gxwf/server-common/src/ast/types";
+import { ASTNode, ObjectASTNode } from "@gxwf/server-common/src/ast/types";
 import { Diagnostic, DiagnosticSeverity, Range, WorkflowDocument } from "@gxwf/server-common/src/languageTypes";
 import { SchemaNode, SchemaNodeResolver } from "../schema";
-import { RecordSchemaNode, FieldSchemaNode, IdMapper } from "../schema/definitions";
+import { RecordSchemaNode, IdMapper } from "../schema/definitions";
 
 export class GxFormat2SchemaValidationService {
   constructor(protected readonly schemaNodeResolver: SchemaNodeResolver) {}
 
   public doValidation(workflowDocument: WorkflowDocument): Promise<Diagnostic[]> {
     const diagnostics: Diagnostic[] = [];
+    this.collectSchemaDiagnostics(workflowDocument, diagnostics);
+    return Promise.resolve(diagnostics);
+  }
+
+  private collectSchemaDiagnostics(workflowDocument: WorkflowDocument, diagnostics: Diagnostic[]): void {
     if (workflowDocument.nodeManager.root) {
       this.collectDiagnostics(
         workflowDocument.nodeManager,
         workflowDocument.nodeManager.root,
         this.schemaNodeResolver.rootNode,
-        null,
         diagnostics
       );
     }
-    return Promise.resolve(diagnostics);
   }
 
   private collectDiagnostics(
     nodeManager: ASTNodeManager,
     node: ASTNode,
     schemaNode: SchemaNode,
-    parenSchemaNode: SchemaNode | null,
-    diagnostics: Diagnostic[]
+    diagnostics: Diagnostic[],
+    parenSchemaNode?: SchemaNode
   ): void {
     const range = this.getRange(nodeManager, node);
     if (schemaNode instanceof RecordSchemaNode) {
-      if (node.type !== "object") {
-        if (schemaNode.matchesTypeField(node.type)) {
-          return;
-        }
-        if (schemaNode.matchesMapping(node.type, parenSchemaNode as IdMapper)) {
-          return;
-        }
-        diagnostics.push(Diagnostic.create(range, `${schemaNode.name} definition expected`, DiagnosticSeverity.Error));
-      } else {
-        schemaNode.fields.forEach((schemaFieldNode) => {
-          const propertyNode = node.properties.find((prop) => prop.keyNode.value === schemaFieldNode.name);
-          const nodeFound = propertyNode !== undefined;
-          if (schemaFieldNode.isRequired && !nodeFound) {
-            diagnostics.push(Diagnostic.create(range, `${schemaFieldNode.name} is required`, DiagnosticSeverity.Error));
-          }
-          if (nodeFound) {
-            const childSchemaNode = this.schemaNodeResolver.getSchemaNodeByTypeRef(schemaFieldNode.typeRef);
-            if (childSchemaNode && propertyNode.valueNode) {
-              if (schemaFieldNode.supportsArray) {
-                propertyNode.valueNode.children?.forEach((item) => {
-                  if (item.type === "property" && item.valueNode) {
-                    this.collectDiagnostics(nodeManager, item.valueNode, childSchemaNode, schemaFieldNode, diagnostics);
-                  }
-                });
-              } else {
-                this.collectDiagnostics(
-                  nodeManager,
-                  propertyNode.valueNode,
-                  childSchemaNode,
-                  schemaFieldNode,
-                  diagnostics
-                );
-              }
-            }
-          }
-        });
+      switch (node.type) {
+        case "object":
+          this.validateObjectNode(nodeManager, node, schemaNode, range, diagnostics);
+          break;
+
+        default:
+          this.validateNodeTypeDefinition(schemaNode, node, range, diagnostics, parenSchemaNode);
+          break;
       }
-    } else if (schemaNode instanceof FieldSchemaNode) {
-      console.log("Field Schema Node");
-    } else {
-      console.log("Other Schema Node");
+    }
+  }
+
+  private validateObjectNode(
+    nodeManager: ASTNodeManager,
+    node: ObjectASTNode,
+    schemaNode: RecordSchemaNode,
+    range: Range,
+    diagnostics: Diagnostic[]
+  ): void {
+    schemaNode.fields.forEach((schemaFieldNode) => {
+      const propertyNode = node.properties.find((prop) => prop.keyNode.value === schemaFieldNode.name);
+      const nodeFound = propertyNode !== undefined;
+      if (schemaFieldNode.isRequired && !nodeFound) {
+        diagnostics.push(Diagnostic.create(range, `${schemaFieldNode.name} is required`, DiagnosticSeverity.Error));
+      }
+      if (nodeFound) {
+        const childSchemaNode = this.schemaNodeResolver.getSchemaNodeByTypeRef(schemaFieldNode.typeRef);
+        if (childSchemaNode && propertyNode.valueNode) {
+          if (schemaFieldNode.supportsArray) {
+            propertyNode.valueNode.children?.forEach((item) => {
+              if (item.type === "property" && item.valueNode) {
+                this.collectDiagnostics(nodeManager, item.valueNode, childSchemaNode, diagnostics, schemaFieldNode);
+              }
+            });
+          } else {
+            this.collectDiagnostics(nodeManager, propertyNode.valueNode, childSchemaNode, diagnostics, schemaFieldNode);
+          }
+        }
+      }
+    });
+  }
+
+  private validateNodeTypeDefinition(
+    schemaNode: RecordSchemaNode,
+    node: ASTNode,
+    range: Range,
+    diagnostics: Diagnostic[],
+    parenSchemaNode?: SchemaNode
+  ): void {
+    if (!schemaNode.matchesTypeField(node.type) && !schemaNode.matchesMapping(node.type, parenSchemaNode as IdMapper)) {
+      diagnostics.push(Diagnostic.create(range, `${schemaNode.name} definition expected`, DiagnosticSeverity.Error));
     }
   }
 
