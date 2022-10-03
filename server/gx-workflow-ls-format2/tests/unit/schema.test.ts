b3827042df4296b9621142abe07dd2bc5cab745a
davelopez <46503462+davelopez@users.noreply.github.com>
2022-10-03 22:35:34 +0200

    Add more tests for schema definitions

--- server/gx-workflow-ls-format2/tests/unit/schema.test.ts
+++ server/gx-workflow-ls-format2/tests/unit/schema.test.ts
@@ -1,5 +1,5 @@
 import { NodePath } from "@gxwf/server-common/src/ast/types";
-import { GalaxyWorkflowFormat2SchemaLoader, SchemaNodeResolver } from "../../src/schema";
+import { GalaxyWorkflowFormat2SchemaLoader, RecordSchemaNode, SchemaNodeResolver } from "../../src/schema";
 
 describe("Gxformat2 Schema Handling", () => {
   describe("Schema Version 19_09", () => {
@@ -41,45 +41,156 @@ describe("Gxformat2 Schema Handling", () => {
     });
 
     describe("Definitions", () => {
-      it("should define GalaxyWorkflow record as expected", () => {
-        const record = schemaLoader.definitions.records.get("GalaxyWorkflow");
-        expect(record).toBeDefined();
-        expect(record?.name).toBe("GalaxyWorkflow");
-        expect(record?.documentation).toBeDefined();
-        expect(record?.fields.length).toBe(13);
-        const field = record?.getFieldByName("class");
+      function expectRecordHasOptionalStringField(
+        record: RecordSchemaNode,
+        fieldName: string,
+        canBeArray = false
+      ): void {
+        const field = record.getFieldByName(fieldName);
         expect(field).toBeDefined();
+        expect(field?.isOptional).toBe(true);
+        expect(field?.canBeArray).toBe(canBeArray);
+        expect(field?.canBeObject).toBe(false);
         expect(field?.typeRef).toBe("string");
+      }
+      describe("GalaxyWorkflow Record", () => {
+        it("should have the expected field definitions", () => {
+          const record = schemaLoader.definitions.records.get("GalaxyWorkflow") as RecordSchemaNode;
+          expect(record).toBeDefined();
+          expect(record.name).toBe("GalaxyWorkflow");
+          expect(record.documentation).toBeDefined();
+          expect(record.fields.length).toBe(13);
+          let field = record.getFieldByName("class");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(false);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.typeRef).toBe("string");
+          field = record.getFieldByName("steps");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(false);
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.typeRef).toBe("WorkflowStep");
+          field = record.getFieldByName("report");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(true);
+          expect(field?.typeRef).toBe("Report");
+          field = record.getFieldByName("tags");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.typeRef).toBe("string");
+          field = record.getFieldByName("creator");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(true);
+          expect(field?.typeRef).toBe("Any");
+          field = record.getFieldByName("license");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.typeRef).toBe("string");
+          field = record.getFieldByName("release");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.typeRef).toBe("string");
+
+          // HasUUID
+          expectRecordHasOptionalStringField(record, "uuid");
+
+          expectRecordHasOptionalStringField(record, "id");
+          expectRecordHasOptionalStringField(record, "label");
+          expectRecordHasOptionalStringField(record, "doc", true);
+        });
       });
 
-      it("should define WorkflowStep record as expected", () => {
-        const record = schemaLoader.definitions.records.get("WorkflowStep");
-        expect(record).toBeDefined();
-        expect(record?.name).toBe("WorkflowStep");
-        expect(record?.documentation).toBeDefined();
-        expect(record?.fields.length).toBe(16);
-        let field = record?.getFieldByName("in");
-        expect(field).toBeDefined();
-        expect(field?.isOptional).toBe(true);
-        expect(field?.canBeArray).toBe(true);
-        expect(field?.matchesType("null")).toBe(true);
-        expect(field?.matchesType("WorkflowStepInput")).toBe(true);
-        field = record?.getFieldByName("out");
-        expect(field).toBeDefined();
-        expect(field?.isOptional).toBe(true);
-        expect(field?.canBeArray).toBe(true);
-        expect(field?.matchesType("null")).toBe(true);
-        expect(field?.matchesType("string")).toBe(true);
-        expect(field?.matchesType("WorkflowStepOutput")).toBe(true);
-        expect(field?.matchesType("Any")).toBe(false);
-        field = record?.getFieldByName("state");
-        expect(field).toBeDefined();
-        expect(field?.isOptional).toBe(true);
-        expect(field?.typeRef).toBe("Any");
-        expect(field?.canBeArray).toBe(true);
-        expect(field?.matchesType("null")).toBe(true);
-        expect(field?.matchesType("string")).toBe(true);
-        expect(field?.matchesType("anything...")).toBe(true);
+      describe("WorkflowStep Record", () => {
+        it("should have the expected field definitions", () => {
+          const record = schemaLoader.definitions.records.get("WorkflowStep") as RecordSchemaNode;
+          expect(record).toBeDefined();
+          expect(record.name).toBe("WorkflowStep");
+          expect(record.documentation).toBeDefined();
+          expect(record.fields.length).toBe(16);
+          let field = record.getFieldByName("in");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.matchesType("null")).toBe(true);
+          expect(field?.matchesType("WorkflowStepInput")).toBe(true);
+          field = record.getFieldByName("out");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.matchesType("null")).toBe(true);
+          expect(field?.matchesType("string")).toBe(true);
+          expect(field?.matchesType("WorkflowStepOutput")).toBe(true);
+          expect(field?.matchesType("Any")).toBe(false);
+          field = record.getFieldByName("state");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.typeRef).toBe("Any");
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(true);
+          expect(field?.matchesType("null")).toBe(true);
+          expect(field?.matchesType("string")).toBe(true);
+          expect(field?.matchesType("anything...")).toBe(true);
+          field = record.getFieldByName("type");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.default).toBe("tool");
+          field = record.getFieldByName("run");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(true);
+          expect(field?.typeRef).toBe("GalaxyWorkflow");
+          field = record.getFieldByName("runtime_inputs");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(true);
+          expect(field?.canBeObject).toBe(false);
+          expect(field?.typeRef).toBe("string");
+
+          expectRecordHasOptionalStringField(record, "id");
+          expectRecordHasOptionalStringField(record, "label");
+          expectRecordHasOptionalStringField(record, "doc", true);
+
+          // HasStepPosition
+          field = record.getFieldByName("position");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(true);
+          expect(field?.typeRef).toBe("StepPosition");
+
+          // ReferencesTool
+          expectRecordHasOptionalStringField(record, "tool_id");
+          expectRecordHasOptionalStringField(record, "tool_version");
+          field = record.getFieldByName("tool_shed_repository");
+          expect(field).toBeDefined();
+          expect(field?.isOptional).toBe(true);
+          expect(field?.canBeArray).toBe(false);
+          expect(field?.canBeObject).toBe(true);
+          expect(field?.typeRef).toBe("ToolShedRepository");
+
+          // HasStepErrors
+          expectRecordHasOptionalStringField(record, "errors");
+
+          // HasUUID
+          expectRecordHasOptionalStringField(record, "uuid");
+        });
       });
     });
   });
