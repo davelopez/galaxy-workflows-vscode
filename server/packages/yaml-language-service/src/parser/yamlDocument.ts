1397b7b0d8864038263794d2513c48b17cc7bb58
davelopez <46503462+davelopez@users.noreply.github.com>
2022-10-04 16:14:14 +0200

    Fix getNodeFromOffset in some contexts
    This makes finding the context node at cursor a bit more accurate.

--- server/packages/yaml-language-service/src/parser/yamlDocument.ts
+++ server/packages/yaml-language-service/src/parser/yamlDocument.ts
@@ -70,6 +70,9 @@ export class YAMLDocument implements ParsedDocument {
     const position = this._textBuffer.getPosition(offset);
     if (position.character === 0 && !this._textBuffer.hasTextAfterPosition(position)) return rootNode;
     const indentation = this._textBuffer.getLineIndentationAtOffset(offset);
+    const lineContent = this._textBuffer.getLineContent(position.line);
+    const contentAfterCursor = lineContent.slice(position.character).replace(/\s/g, "");
+    if (indentation === 0 && contentAfterCursor.length === 0) return rootNode;
     let result = rootNode.getNodeFromOffsetEndInclusive(offset);
     const parent = this.findParentNodeByIndentation(offset, indentation);
     if (!result || (parent && result.offset < parent.offset && result.length > parent.length)) {
--- server/packages/yaml-language-service/tests/unit/yamlParser.test.ts
+++ server/packages/yaml-language-service/tests/unit/yamlParser.test.ts
@@ -182,9 +182,9 @@ test02:
     it.each([
       [0, "_root_"], // _root_ is not a property, is an object
       [1, "test"],
-      [12, "test"],
+      [12, "_root_"],
       [13, "test02"],
-      [20, "test02"],
+      [20, "_root_"],
       [22, "test02"],
       [23, "prop03"],
       [30, "prop03"],
