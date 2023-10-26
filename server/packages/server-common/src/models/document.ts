1d24f7988291e85f4f52da4d61288f0f9c1e4d89
davelopez <46503462+davelopez@users.noreply.github.com>
2023-10-26 21:33:11 +0200

    Extract base and add WorkflowTestsDocument

---
+++ server/packages/server-common/src/models/document.ts
@@ -0,0 +1,34 @@
+import { DocumentContext, TextDocument } from "../languageTypes";
+import { URI } from "vscode-uri";
+import { ParsedDocument } from "../ast/types";
+import { ASTNodeManager } from "../ast/nodeManager";
+
+/**
+ * This class contains basic common handling logic for any kind of known document.
+ */
+export abstract class DocumentBase implements DocumentContext {
+  protected _textDocument: TextDocument;
+  protected _documentUri: URI;
+  protected _parsedDocument: ParsedDocument;
+  protected _nodeManager: ASTNodeManager;
+
+  constructor(textDocument: TextDocument, parsedDocument: ParsedDocument) {
+    this._textDocument = textDocument;
+    this._parsedDocument = parsedDocument;
+    this._nodeManager = new ASTNodeManager(textDocument, parsedDocument);
+    this._documentUri = URI.parse(this._textDocument.uri);
+  }
+
+  public get uri(): URI {
+    return this._documentUri;
+  }
+
+  public get textDocument(): TextDocument {
+    return this._textDocument;
+  }
+
+  /** Abstract Syntax Tree Node Manager associated with this document. */
+  public get nodeManager(): ASTNodeManager {
+    return this._nodeManager;
+  }
+}
--- server/packages/server-common/src/models/workflowDocument.ts
+++ server/packages/server-common/src/models/workflowDocument.ts
@@ -1,34 +1,8 @@
-import { DocumentContext, TextDocument } from "../languageTypes";
-import { URI } from "vscode-uri";
-import { ParsedDocument } from "../ast/types";
-import { ASTNodeManager } from "../ast/nodeManager";
+import { DocumentBase } from "./document";
 
 /**
- * This class contains information about workflow semantics.
+ * This class abstracts the common logic of workflow documents.
  */
-export abstract class WorkflowDocument implements DocumentContext {
-  protected _textDocument: TextDocument;
-  protected _documentUri: URI;
-  protected _parsedDocument: ParsedDocument;
-  protected _nodeManager: ASTNodeManager;
-
-  constructor(textDocument: TextDocument, parsedDocument: ParsedDocument) {
-    this._textDocument = textDocument;
-    this._parsedDocument = parsedDocument;
-    this._nodeManager = new ASTNodeManager(textDocument, parsedDocument);
-    this._documentUri = URI.parse(this._textDocument.uri);
-  }
-
-  public get uri(): URI {
-    return this._documentUri;
-  }
-
-  public get textDocument(): TextDocument {
-    return this._textDocument;
-  }
-
-  /** Abstract Syntax Tree Node Manager associated with this document. */
-  public get nodeManager(): ASTNodeManager {
-    return this._nodeManager;
-  }
+export abstract class WorkflowDocument extends DocumentBase {
+  // TODO: add workflow document specific logic
 }
