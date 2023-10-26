c747badf10c4f3608836212d3205c8ae853c1435
davelopez <46503462+davelopez@users.noreply.github.com>
2023-10-26 21:33:11 +0200

    Extract base and add WorkflowTestsDocument

---
+++ server/packages/server-common/src/models/workflowTestsDocument.ts
@@ -0,0 +1,8 @@
+import { DocumentBase } from "./document";
+
+/**
+ * This class contains information about a document containing workflow tests.
+ */
+export abstract class WorkflowTestsDocument extends DocumentBase {
+  // TODO: implement workflow test document specific logic
+}
