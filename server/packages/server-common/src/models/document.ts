8a4e9f71a54b54d5e241ccddc95e1711208ef0cb
davelopez <46503462+davelopez@users.noreply.github.com>
2023-10-29 17:39:20 +0100

    Refactor DocumentBase

--- server/packages/server-common/src/models/document.ts
+++ server/packages/server-common/src/models/document.ts
@@ -7,32 +7,18 @@ import { ASTNodeManager } from "../ast/nodeManager";
  * This class contains basic common handling logic for any kind of known document.
  */
 export abstract class DocumentBase implements DocumentContext {
-  protected _textDocument: TextDocument;
-  protected _documentUri: URI;
-  protected _parsedDocument: ParsedDocument;
-  protected _nodeManager: ASTNodeManager;
+  public readonly uri: URI;
+  public readonly nodeManager: ASTNodeManager;
 
-  constructor(textDocument: TextDocument, parsedDocument: ParsedDocument) {
-    this._textDocument = textDocument;
-    this._parsedDocument = parsedDocument;
-    this._nodeManager = new ASTNodeManager(textDocument, parsedDocument);
-    this._documentUri = URI.parse(this._textDocument.uri);
+  constructor(
+    public readonly textDocument: TextDocument,
+    protected readonly parsedDocument: ParsedDocument
+  ) {
+    this.nodeManager = new ASTNodeManager(textDocument, parsedDocument);
+    this.uri = URI.parse(textDocument.uri);
   }
 
   public get languageId(): string {
-    return this._textDocument.languageId;
-  }
-
-  public get uri(): URI {
-    return this._documentUri;
-  }
-
-  public get textDocument(): TextDocument {
-    return this._textDocument;
-  }
-
-  /** Abstract Syntax Tree Node Manager associated with this document. */
-  public get nodeManager(): ASTNodeManager {
-    return this._nodeManager;
+    return this.textDocument.languageId;
   }
 }
