6650dfdc4f225df96a994849420572b4d864cb07
davelopez <46503462+davelopez@users.noreply.github.com>
2023-11-29 17:19:40 +0100

    Remove unnecessary reference and import in versions.ts

--- server/gx-workflow-ls-format2/src/schema/versions.ts
+++ server/gx-workflow-ls-format2/src/schema/versions.ts
@@ -1,7 +1,3 @@
-//TODO: remove this reference when https://github.com/sumwatshade/jest-transform-yaml/pull/20 is merged and upgrade to jest 28
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../types.d.ts" />
-
 import schema_v19_09_workflows from "@schemas/gxformat2/v19_09/workflows.yaml";
 import schema_common_metaschema_base from "@schemas/gxformat2/common/metaschema/metaschema_base.yaml";
 import schema_v19_09_process from "@schemas/gxformat2/v19_09/process.yaml";
