d61e90930cb6d43400e3d7a27bd5eb94d10f995c
davelopez <46503462+davelopez@users.noreply.github.com>
2023-12-10 15:48:30 +0100

    Move schema provider under schema

--- server/packages/workflow-tests-language-service/src/schema/provider.ts
+++ server/packages/workflow-tests-language-service/src/schema/provider.ts
@@ -1,14 +1,14 @@
 import { injectable } from "inversify";
-import WorkflowTestsSchema from "../../../../workflow-languages/schemas/tests.schema.json";
+import WorkflowTestsSchema from "../../../../../workflow-languages/schemas/tests.schema.json";
 import { JSONSchema } from "vscode-json-languageservice";
 
 export interface WorkflowTestsSchemaProvider {
-  getSchema(): Promise<JSONSchema>;
+  getSchema(): JSONSchema;
 }
 
 @injectable()
 export class WorkflowTestsSchemaProviderImpl implements WorkflowTestsSchemaProvider {
-  public async getSchema(): Promise<JSONSchema> {
+  public getSchema(): JSONSchema {
     return WorkflowTestsSchema;
   }
 }
