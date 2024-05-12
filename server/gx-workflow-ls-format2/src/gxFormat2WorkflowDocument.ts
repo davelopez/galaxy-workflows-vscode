beab66f926b199eaf17ba317d898e9c59a49ffd1
davelopez <46503462+davelopez@users.noreply.github.com>
2024-05-12 02:44:55 +0200

    Fix input type parsing for format2 documents

--- server/gx-workflow-ls-format2/src/gxFormat2WorkflowDocument.ts
+++ server/gx-workflow-ls-format2/src/gxFormat2WorkflowDocument.ts
@@ -1,7 +1,11 @@
 import { PropertyASTNode } from "@gxwf/server-common/src/ast/types";
 import { TextDocument, WorkflowDocument } from "@gxwf/server-common/src/languageTypes";
 import { YAMLDocument } from "@gxwf/yaml-language-service/src";
-import { GetWorkflowInputsResult, GetWorkflowOutputsResult } from "../../../shared/src/requestsDefinitions";
+import {
+  GetWorkflowInputsResult,
+  GetWorkflowOutputsResult,
+  WorkflowInputType,
+} from "../../../shared/src/requestsDefinitions";
 
 /**
  * This class provides information about a gxformat2 workflow document structure.
@@ -26,22 +30,16 @@ export class GxFormat2WorkflowDocument extends WorkflowDocument {
         inputList.forEach((input) => {
           if (input.type !== "property" || !input.keyNode) return;
           const inputName = String(input.keyNode.value);
-          const inputTypeNode = input.valueNode?.children?.find(
-            (prop) => prop.type === "property" && prop.keyNode.value === "type"
-          ) as PropertyASTNode;
-          const inputType =
-            !inputTypeNode || inputTypeNode.valueNode?.value === "File" ? "data_input" : "data_collection_input";
+          const inputType = this.getInputType(input);
           const inputDocNode = input.valueNode?.children?.find(
             (prop) => prop.type === "property" && prop.keyNode.value === "doc"
           ) as PropertyASTNode;
           const inputDescription = String(inputDocNode?.valueNode?.value ?? "");
-          if (inputType) {
-            result.inputs.push({
-              name: inputName,
-              description: inputDescription,
-              type: inputType,
-            });
-          }
+          result.inputs.push({
+            name: inputName,
+            description: inputDescription,
+            type: inputType,
+          });
         });
       }
     }
@@ -50,5 +48,19 @@ export class GxFormat2WorkflowDocument extends WorkflowDocument {
 
   public getWorkflowOutputs(): GetWorkflowOutputsResult {
     throw new Error("Method not implemented.");
+  }
+
+  private getInputType(input: PropertyASTNode): WorkflowInputType {
+    let inputType: WorkflowInputType = "data";
+    const inputTypeNode = input.valueNode?.children?.find(
+      (prop) => prop.type === "property" && prop.keyNode.value === "type"
+    ) as PropertyASTNode;
+    if (inputTypeNode) {
+      inputType = String(inputTypeNode.valueNode?.value) as WorkflowInputType;
+    } else {
+      // If the type property is not specified, it might be defined in the value node itself
+      inputType = input.valueNode?.value as WorkflowInputType;
+    }
+    return inputType;
   }
 }
