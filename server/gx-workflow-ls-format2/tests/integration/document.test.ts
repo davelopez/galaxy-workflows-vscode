71dfa85c22e2e2701f1fc6097f31ec9eabe18ffb
davelopez <46503462+davelopez@users.noreply.github.com>
2024-05-12 02:25:46 +0200

    Add test for getting format2 workflow inputs

--- server/gx-workflow-ls-format2/tests/integration/document.test.ts
+++ server/gx-workflow-ls-format2/tests/integration/document.test.ts
@@ -2,35 +2,53 @@ import { createFormat2WorkflowDocument } from "../testHelpers";
 
 describe("GxFormat2WorkflowDocument", () => {
   it("should get workflow inputs", () => {
-    const documentContent = `
+    const TEST_WORKFLOW_CONTENT = `
 class: GalaxyWorkflow
 inputs:
   input_1: data
   input_2:
     type: File
     doc: This is the input 2
-outputs:
-  output_1:
-    outputSource: first_cat/out_file1
-steps:
-  first_cat:
-    tool_id: cat
-    in:
-      input1: input_1
+  the_collection:
+    type: collection
+    doc: This is a collection
+  input_int: integer
+  text_param:
+    optional: true
+    default: text value
+    restrictOnConnections: true
+    type: text
     `;
-    const document = createFormat2WorkflowDocument(documentContent);
+    const document = createFormat2WorkflowDocument(TEST_WORKFLOW_CONTENT);
+
     const result = document.getWorkflowInputs();
-    expect(result.inputs.length).toBe(2);
+
+    expect(result.inputs.length).toBe(5);
     expect(result.inputs).toEqual([
       {
         name: "input_1",
         description: "",
-        type: "data_input",
+        type: "data",
       },
       {
         name: "input_2",
         description: "This is the input 2",
-        type: "data_input",
+        type: "File",
+      },
+      {
+        name: "the_collection",
+        description: "This is a collection",
+        type: "collection",
+      },
+      {
+        name: "input_int",
+        description: "",
+        type: "integer",
+      },
+      {
+        name: "text_param",
+        description: "",
+        type: "text",
       },
     ]);
   });
