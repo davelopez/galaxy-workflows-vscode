f64009637096aad193592ca6a36eff5f21237f4e
davelopez <46503462+davelopez@users.noreply.github.com>
2024-05-15 11:28:33 +0200

    Add test for native wf inputs

--- server/gx-workflow-ls-native/tests/unit/nativeWorkflowDocument.test.ts
+++ server/gx-workflow-ls-native/tests/unit/nativeWorkflowDocument.test.ts
@@ -14,4 +14,36 @@ describe("NativeWorkflowDocument", () => {
       expect(stepNodes).toHaveLength(expectedNumSteps);
     });
   });
+  describe("getWorkflowInputs", () => {
+    it.each([
+      ["", 0],
+      [TestWorkflowProvider.workflows.validation.withoutSteps, 0],
+      [TestWorkflowProvider.workflows.validation.withOneStep, 1],
+      [TestWorkflowProvider.workflows.validation.withThreeSteps, 2],
+      [TestWorkflowProvider.workflows.validation.withWorkflowOutputLabels, 1],
+      [TestWorkflowProvider.workflows.validation.withoutWorkflowOutputLabels, 1],
+    ])("returns the expected number of inputs", (wf_content: string, expectedNumInputs: number) => {
+      const document = createNativeWorkflowDocument(wf_content);
+      const result = document.getWorkflowInputs();
+      expect(result.inputs).toHaveLength(expectedNumInputs);
+    });
+
+    it("should return the expected information of the inputs", () => {
+      const document = createNativeWorkflowDocument(TestWorkflowProvider.workflows.validation.withThreeSteps);
+      const result = document.getWorkflowInputs();
+      expect(result.inputs).toHaveLength(2);
+      expect(result.inputs).toEqual([
+        {
+          name: "WorkflowInput1",
+          doc: "input1 description",
+          type: "data",
+        },
+        {
+          name: "WorkflowInput2",
+          doc: "",
+          type: "data",
+        },
+      ]);
+    });
+  });
 });
