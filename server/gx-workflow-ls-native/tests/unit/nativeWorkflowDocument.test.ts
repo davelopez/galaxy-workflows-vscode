01995fe6a67cb949488bfadeecab2a2d05c0c2fd
davelopez <46503462+davelopez@users.noreply.github.com>
2024-05-24 18:17:07 +0200

    Add tests for getting workflow outputs

--- server/gx-workflow-ls-native/tests/unit/nativeWorkflowDocument.test.ts
+++ server/gx-workflow-ls-native/tests/unit/nativeWorkflowDocument.test.ts
@@ -14,6 +14,7 @@ describe("NativeWorkflowDocument", () => {
       expect(stepNodes).toHaveLength(expectedNumSteps);
     });
   });
+
   describe("getWorkflowInputs", () => {
     it.each([
       ["", 0],
@@ -42,6 +43,54 @@ describe("NativeWorkflowDocument", () => {
           name: "WorkflowInput2",
           doc: "",
           type: "data",
+        },
+      ]);
+    });
+  });
+
+  describe("getWorkflowOutputs", () => {
+    it.each([
+      ["", 0],
+      [TestWorkflowProvider.workflows.validation.withoutSteps, 0],
+      [TestWorkflowProvider.workflows.validation.withOneStep, 0],
+      [TestWorkflowProvider.workflows.validation.withThreeSteps, 0],
+      [TestWorkflowProvider.workflows.validation.withWorkflowOutputLabels, 2],
+      [TestWorkflowProvider.workflows.validation.withoutWorkflowOutputLabels, 2],
+    ])("returns the expected number of outputs", (wf_content: string, expectedNumInputs: number) => {
+      const document = createNativeWorkflowDocument(wf_content);
+      const result = document.getWorkflowOutputs();
+      expect(result.outputs).toHaveLength(expectedNumInputs);
+    });
+
+    it("should return the expected information of the outputs with labels", () => {
+      const document = createNativeWorkflowDocument(TestWorkflowProvider.workflows.validation.withWorkflowOutputLabels);
+      const result = document.getWorkflowOutputs();
+      expect(result.outputs).toHaveLength(2);
+      expect(result.outputs).toEqual([
+        {
+          name: "The first output",
+          uuid: "7f08baab-5426-427e-9640-85815d809261",
+        },
+        {
+          name: "The second output",
+          uuid: "b58fce9c-e507-4714-abfc-739607e02eed",
+        },
+      ]);
+    });
+    it("should return the expected information of the outputs without labels", () => {
+      const document = createNativeWorkflowDocument(
+        TestWorkflowProvider.workflows.validation.withoutWorkflowOutputLabels
+      );
+      const result = document.getWorkflowOutputs();
+      expect(result.outputs).toHaveLength(2);
+      expect(result.outputs).toEqual([
+        {
+          name: "output1",
+          uuid: "7f08baab-5426-427e-9640-85815d809261",
+        },
+        {
+          name: "output2",
+          uuid: "b58fce9c-e507-4714-abfc-739607e02eed",
         },
       ]);
     });
