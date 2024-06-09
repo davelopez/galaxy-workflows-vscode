f226f95da39a0d5d5f0ca48b198813e57137c2c1
davelopez <46503462+davelopez@users.noreply.github.com>
2024-06-09 10:24:37 +0200

    Improve document detection
    Detect not only .yml but also .yaml document extensions.

--- client/src/requests/gxworkflows.ts
+++ client/src/requests/gxworkflows.ts
@@ -12,6 +12,8 @@ import {
   TargetWorkflowDocumentParams,
 } from "../languageTypes";
 
+const TEST_DOCUMENT_TO_WORKFLOW_DOCUMENT_URI_MAP = new Map<string, Uri>();
+
 export function setupRequests(
   context: ExtensionContext,
   nativeWorkflowClient: BaseLanguageClient,
@@ -19,23 +21,34 @@ export function setupRequests(
 ): void {
   function createRequestHandler<TResult>(requestIdentifier: string) {
     return async (params: TargetWorkflowDocumentParams) => {
-      let targetUri: Uri | undefined = Uri.parse(params.uri);
+      const targetUri: Uri | undefined = Uri.parse(params.uri);
+      let resultUri: Uri | undefined = undefined;
       if (isWorkflowTestsDocument(targetUri)) {
         // If the target is a test file, we need to find the associated workflow file
-        targetUri = await getAssociatedWorkflowUriFromTestsUri(targetUri);
+        //Try cache first
+        const cacheKey = targetUri.toString();
+        if (TEST_DOCUMENT_TO_WORKFLOW_DOCUMENT_URI_MAP.has(cacheKey)) {
+          resultUri = TEST_DOCUMENT_TO_WORKFLOW_DOCUMENT_URI_MAP.get(cacheKey)!;
+        }
+        if (!resultUri) {
+          resultUri = await getAssociatedWorkflowUriFromTestsUri(targetUri);
+          if (resultUri) {
+            TEST_DOCUMENT_TO_WORKFLOW_DOCUMENT_URI_MAP.set(cacheKey, resultUri);
+          }
+        }
       }
-      if (!targetUri) {
+      if (!resultUri) {
         console.debug("No associated workflow file found for:", params.uri);
         return undefined;
       }
       // Open the file to include it in the document cache
-      await workspace.openTextDocument(targetUri);
+      await workspace.openTextDocument(resultUri);
 
       let languageClient = gxFormat2Client;
-      if (isNativeWorkflowDocument(targetUri)) {
+      if (isNativeWorkflowDocument(resultUri)) {
         languageClient = nativeWorkflowClient;
       }
-      const requestParams: TargetWorkflowDocumentParams = { uri: targetUri.toString() };
+      const requestParams: TargetWorkflowDocumentParams = { uri: resultUri.toString() };
       const result = await languageClient.sendRequest<TResult>(requestIdentifier, requestParams);
       return result;
     };
