4860bc5e23fa60efd08ff06c8244129c51eb3354
davelopez <46503462+davelopez@users.noreply.github.com>
2024-06-16 20:55:48 +0200

    Refactor validation rule tests for wf test documents

--- server/packages/workflow-tests-language-service/tests/unit/validation.test.ts
+++ server/packages/workflow-tests-language-service/tests/unit/validation.test.ts
@@ -1,10 +1,17 @@
 import { container } from "@gxwf/server-common/src/inversify.config";
-import { Diagnostic, DiagnosticSeverity, WorkflowDataProvider } from "@gxwf/server-common/src/languageTypes";
+import {
+  Diagnostic,
+  DiagnosticSeverity,
+  ValidationRule,
+  WorkflowDataProvider,
+} from "@gxwf/server-common/src/languageTypes";
 import { FAKE_WORKFLOW_DATA_PROVIDER } from "@gxwf/server-common/tests/testHelpers";
 import { WorkflowTestsLanguageServiceContainerModule } from "@gxwf/workflow-tests-language-service/src/inversify.config";
 import { WorkflowTestsValidationService } from "@gxwf/workflow-tests-language-service/src/services/validation";
 import "reflect-metadata";
 import { TYPES } from "../../src/types";
+import { WorkflowInputsValidationRule } from "../../src/validation/rules/WorkflowInputsValidationRule";
+import { WorkflowOutputsValidationRule } from "../../src/validation/rules/WorkflowOutputsValidationRule";
 import { createGxWorkflowTestsDocument } from "../testHelpers";
 
 describe("Workflow Tests Validation Service", () => {
@@ -35,15 +42,30 @@ describe("Workflow Tests Validation Service", () => {
     expect(diagnostics[1].message).toBe('Missing property "outputs".');
     expect(diagnostics[1].severity).toBe(DiagnosticSeverity.Warning);
   });
+});
 
-  describe("Workflow Inputs/Outputs Validation", () => {
-    it("should pass validation when the inputs and outputs are defined in the workflow", async () => {
+describe("Workflow Tests Validation Rules", () => {
+  let rule: ValidationRule;
+
+  async function validate(
+    contents: string,
+    workflowDataProvider: WorkflowDataProvider = FAKE_WORKFLOW_DATA_PROVIDER
+  ): Promise<Diagnostic[]> {
+    const documentContext = createGxWorkflowTestsDocument(contents, workflowDataProvider);
+    return await rule.validate(documentContext);
+  }
+
+  describe("WorkflowInputsValidationRule", () => {
+    beforeAll(() => {
+      rule = new WorkflowInputsValidationRule();
+    });
+
+    it("should pass validation when a valid input is defined in the workflow", async () => {
       const testDocumentContents = `
-- doc: The docs
-  job:
-    My fake dataset: data/input.txt
-  outputs:
-    My output: out/output.txt`;
+  - doc: The docs
+    job:
+      My fake dataset: data/input.txt
+    `;
 
       const diagnostics = await validate(testDocumentContents);
 
@@ -52,11 +74,10 @@ describe("Workflow Tests Validation Service", () => {
 
     it("should error when an input is not defined in the workflow", async () => {
       const testDocumentContents = `
-- doc: The docs
-  job:
-    Missing input: data/input.txt
-  outputs:
-    My output: out/output.txt`;
+  - doc: The docs
+    job:
+      Missing input: data/input.txt
+    `;
 
       const diagnostics = await validate(testDocumentContents);
 
@@ -64,12 +85,27 @@ describe("Workflow Tests Validation Service", () => {
       expect(diagnostics[0].message).toBe('Input "Missing input" is not defined in the associated workflow.');
       expect(diagnostics[0].severity).toBe(DiagnosticSeverity.Error);
     });
+  });
+
+  describe("WorkflowOutputsValidationRule", () => {
+    beforeAll(() => {
+      rule = new WorkflowOutputsValidationRule();
+    });
+
+    it("should pass validation when a valid outputs is defined in the workflow", async () => {
+      const testDocumentContents = `
+  - doc: The docs
+    outputs:
+      My output: out/output.txt`;
+
+      const diagnostics = await validate(testDocumentContents);
+
+      expect(diagnostics).not.toBeNull();
+    });
 
     it("should error when an output is not defined in the workflow", async () => {
       const testDocumentContents = `
 - doc: The docs
-  job:
-    My fake dataset: data/input.txt
   outputs:
     Missing output: out/output.txt`;
 
