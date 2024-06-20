d117fb8766a2bc5f4323d1fb69483ae16a20ea05
davelopez <46503462+davelopez@users.noreply.github.com>
2024-06-20 12:52:08 +0200

    Refactor InputTypeValidationRule to skip type validation for invalid types

--- server/gx-workflow-ls-format2/src/validation/rules/InputTypeValidationRule.ts
+++ server/gx-workflow-ls-format2/src/validation/rules/InputTypeValidationRule.ts
@@ -1,4 +1,4 @@
-import { ValidationRule, WorkflowDataType } from "@gxwf/server-common/src/languageTypes";
+import { ValidationRule } from "@gxwf/server-common/src/languageTypes";
 import { isCompatibleType, isWorkflowDataType } from "@gxwf/server-common/src/utils";
 import { Diagnostic, DiagnosticSeverity } from "vscode-languageserver-types";
 import { GxFormat2WorkflowDocument } from "../../gxFormat2WorkflowDocument";
@@ -13,25 +13,21 @@ export class InputTypeValidationRule implements ValidationRule {
     const result: Diagnostic[] = [];
 
     const inputNodes = documentContext.getRawInputNodes();
-    inputNodes.forEach((input) => {
+    for (const input of inputNodes) {
       const inputName = String(input.keyNode.value);
       const inputType = documentContext.nodeManager.getPropertyValueByName(input, "type") as string;
 
       if (!isWorkflowDataType(inputType)) {
-        result.push({
-          message: `Input '${inputName}' has an invalid type. Expected a valid workflow data type but found '${inputType}'.`,
-          range: documentContext.nodeManager.getNodeRange(input),
-          severity: this.severity,
-        });
+        // Skip type validation for invalid types. Those will be caught by the schema validation.
+        continue;
       }
 
       const defaultValueNode = documentContext.nodeManager.getPropertyNodeByName(input, "default");
       const defaultValue = defaultValueNode?.valueNode?.value;
-
       const defaultValueType = typeof defaultValue;
 
       if (inputType && defaultValue) {
-        const defaultTypeMatchesValue = isCompatibleType(inputType as WorkflowDataType, defaultValueType);
+        const defaultTypeMatchesValue = isCompatibleType(inputType, defaultValueType);
         if (!defaultTypeMatchesValue) {
           result.push({
             message: `Input '${inputName}' default value has invalid type. Expected '${inputType}' but found '${defaultValueType}'.`,
@@ -40,8 +36,7 @@ export class InputTypeValidationRule implements ValidationRule {
           });
         }
       }
-    });
-
+    }
     return Promise.resolve(result);
   }
 }
