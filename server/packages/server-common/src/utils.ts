e19d95f8255683fd3eda7aba603bb7db0adf4caf
davelopez <46503462+davelopez@users.noreply.github.com>
2024-06-22 11:03:10 +0200

    Add isSimpleType utility function for type checking

--- server/packages/server-common/src/utils.ts
+++ server/packages/server-common/src/utils.ts
@@ -34,9 +34,26 @@ export function isCompatibleType(expectedType: WorkflowDataType, actualType: str
   return isCompatible;
 }
 
+/**
+ * Check if the type is a valid workflow data type.
+ * @param type The type to check.
+ * @returns True if the type is a valid workflow data type.
+ */
 export function isWorkflowDataType(type?: string): type is WorkflowDataType {
   if (!type) {
     return false;
   }
   return type in workflowDataTypes;
+}
+
+const SIMPLE_TYPES = ["number", "string", "boolean", "null"];
+
+/**
+ * Check if the type is a simple type (i.e. number, string, boolean or null).
+ */
+export function isSimpleType(type?: string): boolean {
+  if (!type) {
+    return false;
+  }
+  return SIMPLE_TYPES.includes(type);
 }
