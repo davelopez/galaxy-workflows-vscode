e037de2b89e255b12be2f93384e9ef06913a70f9
John Chilton <jmchilton@gmail.com>
2026-05-05 23:34:01 -0400

    Wait for CodeLens in e2e test

--- client/tests/e2e/suite/codeLens.e2e.ts
+++ client/tests/e2e/suite/codeLens.e2e.ts
@@ -4,6 +4,21 @@ import * as vscode from "vscode";
 import { activateAndOpenInEditor, closeAllEditors, getDocUri, sleep } from "./helpers";
 import { usePopulatedCache } from "./cacheHelpers";
 
+async function waitForCodeLenses(docUri: vscode.Uri, expectedCount: number): Promise<vscode.CodeLens[]> {
+  const waitMilliseconds = 250;
+  let waitTimeout = 10_000;
+  let lenses: vscode.CodeLens[] = [];
+
+  while (waitTimeout > 0) {
+    lenses = (await vscode.commands.executeCommand<vscode.CodeLens[]>("vscode.executeCodeLensProvider", docUri)) ?? [];
+    if (lenses.length === expectedCount) return lenses;
+    await sleep(waitMilliseconds);
+    waitTimeout -= waitMilliseconds;
+  }
+
+  return lenses;
+}
+
 suite("CodeLens on tool_id", function () {
   teardown(closeAllEditors);
   usePopulatedCache();
@@ -13,11 +28,9 @@ suite("CodeLens on tool_id", function () {
     const docUri = getDocUri(path.join("json", "clean", "iwc_fastp_multiqc_dirty.ga"));
     const ed = await activateAndOpenInEditor(docUri);
     assert.ok(ed);
-    await sleep(500);
 
-    const lenses = await vscode.commands.executeCommand<vscode.CodeLens[]>("vscode.executeCodeLensProvider", docUri);
-    assert.ok(lenses, "expected CodeLens array");
     // iwc_fastp_multiqc_dirty.ga has two tool steps.
+    const lenses = await waitForCodeLenses(docUri, 2);
     assert.strictEqual(lenses.length, 2, `expected 2 lenses, got ${lenses.length}`);
 
     const titles = lenses.map((l) => l.command?.title ?? "");
